807ebd3233f3941631558f0c2fe6496095ece7f5
Sten Hougaard <netsi1964@gmail.com>
2018-03-31 14:02:21 +0200

    Added handling of dynamic port for web server

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ var bodyParser = require("body-parser");
 var routes = require("./routes/routes.js");
 var app = express();
 
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 routes(app);
 
-var server = app.listen(3000, function () {
-    console.log("app running on port.", server.address().port);
+var server = app.listen(port, function () {
+    console.log("app running on port.", port);
 });
