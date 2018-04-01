3f261d60e7c22e7185011d3658380a58c84486fe
Sten Hougaard <netsi1964@gmail.com>
2018-04-01 18:21:42 +0200

    Handling of no params added

--- routes/routes.js
+++ routes/routes.js
@@ -5,27 +5,31 @@ const { JSDOM } = jsdom;
 var appRouter = function(app) {
   app.get("/", function(req, res) {
     let { from, extract } = req.query;
-    extract = JSON.parse(extract);
-    request(from, function(error, response, body) {
-      if (error) {
-        res.status(404).send(`Could not fetch data from ${from}`);
-      } else {
-        const html = new JSDOM(body);
-        const document = html.window.document;
-        let selectors = Object.keys(extract);
-        let values = {};
-        selectors.map(key => {
-          const selector = extract[key];
-          const found = document.querySelector(selector);
-          if (found !== null) {
-            values[key] = found.textContent.trim();
-          } else {
-            values[key] = '';
-          }
-        });
-        res.status(200).send(values);
-      }
-    });
+    if (typeof from !== "undefined" && typeof extract !== "undefined") {
+      extract = JSON.parse(extract);
+      request(from, function(error, response, body) {
+        if (error) {
+          res.status(404).send(`Could not fetch data from ${from}`);
+        } else {
+          const html = new JSDOM(body);
+          const document = html.window.document;
+          let selectors = Object.keys(extract);
+          let values = {};
+          selectors.map(key => {
+            const selector = extract[key];
+            const found = document.querySelector(selector);
+            if (found !== null) {
+              values[key] = found.textContent.trim();
+            } else {
+              values[key] = "";
+            }
+          });
+          res.status(200).send(values);
+        }
+      });
+    } else {
+      res.status(404).send(`Please supply from and extract parameters`);
+    }
   });
 };
 
