0cf58bfa7cfc188c38c6c5811e21765854553275
Sten Hougaard <netsi1964@gmail.com>
2018-04-01 18:08:20 +0200

    Added code to return values

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,31 @@
+const request = require("request");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
 var appRouter = function(app) {
   app.get("/", function(req, res) {
-    console.log(req.query);
-    res.status(200).send({ total: "4,793", likes: "10" });
+    let { from, extract } = req.query;
+    extract = JSON.parse(extract);
+    request(from, function(error, response, body) {
+      if (error) {
+        res.status(404).send(`Could not fetch data from ${from}`);
+      } else {
+        const html = new JSDOM(body);
+        const document = html.window.document;
+        let selectors = Object.keys(extract);
+        let values = {};
+        selectors.map(key => {
+          const selector = extract[key];
+          const found = document.querySelector(selector);
+          if (found !== null) {
+            values[key] = found.textContent.trim();
+          } else {
+            values[key] = '';
+          }
+        });
+        res.status(200).send(values);
+      }
+    });
   });
 };
 
