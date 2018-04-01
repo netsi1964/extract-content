71f375598f18d939b56cbadcc79e6af2be4cfc12
Sten Hougaard <netsi1964@gmail.com>
2018-04-01 12:01:31 +0200

    Added code to enable CORS

--- app.js
+++ app.js
@@ -7,11 +7,20 @@ var app = express();
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 3000;
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 routes(app);
 
-var server = app.listen(port, function () {
-    console.log("app running on port.", port);
+var server = app.listen(port, function() {
+  console.log("app running on port.", port);
 });
